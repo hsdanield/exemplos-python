57f9018cca94208e8c5eb415b6564f7fceb0b7a6
hsdanield <hsdanield@gmail.com>
2022-11-08 15:05:30 -0300

    POO - 3-Getters_Setters_e_Estados

---
+++ POO/3-Getters_Setters_e_Estados/alarme.py
@@ -0,0 +1,11 @@
+class Alarme:
+
+    def __init__(self, estado: bool) -> None:
+        self.__estado = estado
+
+    def get_estado(self) -> bool:
+        return self.__estado
+
+    def set_estado(self, estado: bool) -> None:
+        if isinstance(estado, bool):
+            self.__estado = estado
